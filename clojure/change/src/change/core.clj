3646f292afbd6d570e28b9755b26ad8c5c8d441d
Youri Ulbrich <youri.ulbrich@web.de>
2022-07-24 00:01:22 +0200

    changes implementation

--- clojure/change/src/change/core.clj
+++ clojure/change/src/change/core.clj
@@ -1,6 +1,18 @@
 (ns change.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn div-with-remainder [dividend divisor]
+  ((juxt quot mod) dividend divisor)) 
+
+(defn issue [coin coin-set]
+  (if (or (< coin 0) (neg? coin))
+    (throw (IllegalArgumentException. "cannot change"))
+    (loop [remaining coin
+           change '()
+           remaining-set (filter (partial >= remaining) (sort > coin-set))]
+      (let [current-coin (first remaining-set)
+            [result remainder] (div-with-remainder remaining current-coin)]
+        (if (= remaining 0)
+          (flatten change)
+          (if (empty? remaining-set)
+            (throw (IllegalArgumentException. "cannot change"))
+            (recur remainder (conj change (repeat result current-coin)) (rest remaining-set))))))))
