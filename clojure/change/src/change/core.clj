af6fce851e650e3d580e66e453c6be9572d92c74
Youri <youri.ulbrich@web.de>
2022-09-03 05:10:32 +0200

    Further "change" implementation

--- clojure/change/src/change/core.clj
+++ clojure/change/src/change/core.clj
@@ -3,16 +3,27 @@
 (defn div-with-remainder [dividend divisor]
   ((juxt quot mod) dividend divisor)) 
 
+(defn div-set [set remainder]
+  (map (fn [coin] {coin (div-with-remainder remainder coin)}) set))
+
+(defn smallest-div [divided-set]
+  (apply min-key #(apply second (vals %)) divided-set))
+
 (defn issue [coin coin-set]
   (if (or (< coin 0) (neg? coin))
     (throw (IllegalArgumentException. "cannot change"))
     (loop [remaining coin
            change '()
            remaining-set (filter (partial >= remaining) (sort > coin-set))]
-      (let [current-coin (first remaining-set)
-            [result remainder] (div-with-remainder remaining current-coin)]
-        (if (= remaining 0)
-          (flatten change)
+      (if (= remaining 0)
+        (flatten change)
+        (let [current-coin (first remaining-set)
+              [result remainder] (div-with-remainder remaining current-coin)]
           (if (empty? remaining-set)
             (throw (IllegalArgumentException. "cannot change"))
             (recur remainder (conj change (repeat result current-coin)) (rest remaining-set))))))))
+
+
+
+;'(5 10 21 25) 63
+
