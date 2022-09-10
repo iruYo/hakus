fed27e3044e3809e266cdef8ae1120791bd3bd87
Youri <youri.ulbrich@web.de>
2022-09-10 02:02:02 +0200

    Futher pig-latin implementation

--- clojure/pig-latin/src/pig_latin/core.clj
+++ clojure/pig-latin/src/pig_latin/core.clj
@@ -1,35 +1,38 @@
 (ns pig-latin.core
-  (:require [clojure.set :refer [difference union]]))
+  (:require [clojure.set :refer [difference union]]
+            [clojure.string :as s]))
 
-(def alphabet #{\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z})
-(def vowels #{\a \e \i \o \u})
+(def alphabet #{"a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"})
+(def vowels #{"a" "e" "i" "o" "u"})
 (def consonants (difference alphabet vowels))
-
-(defn vowel-sound? [word]
-  (let [first (first word)
-        second (apply str (take 2 word))
-        vowel-sounds (union vowels #{"xr" "xy"})]
-    (or (contains? vowel-sounds first) (contains? vowel-sounds second))))
-
-(defn consonant-sound? [word]
-  (let [beginning (apply str (take-while #(contains? consonants %) word))]
-    (not (= beginning ""))))
 
 (defn split-consonant-from-word [word]
   (map #(apply str %) (split-with #(contains? consonants %) word)))
 
+(defn starts-with-seq? [word seq]
+  (some #(s/starts-with? word %) seq))
+
+(defn vowel-sound? [word]
+  (let [vowel-sounds (union vowels #{"xr" "xy"})]
+    (starts-with-seq? word vowel-sounds)))
+
+(defn consonant-sound? [word]
+  (starts-with-seq? word consonants))
+
 (defn consonant-sound-with-qu? [word]
-  (if (consonant-sound? word)
-    (map #(apply str %) (split-with #(contains? (union consonants #{"qu"}) %) word))))
+  (when (consonant-sound? word)
+    (when-let [split-word (second (split-consonant-from-word word))]
+      (= "qu" (take 2 split-word)))))
 
 (defn consonant-sound-with-y? [word]
-)
-; (if (some zero? (map (partial mod number) primes))
- (defn translate [word] ;; <- arglist goes here 
-   (cond
-     (vowel-sound? word) (str word "ay")
-     (consonant-sound-with-qu? word) (nil)
-     (consonant-sound-with-y? word) (str word "ay")
-     (consonant-sound? word) (let [word-seq (split-consonant-from-word word)]
-                               (str (second word-seq) (first word-seq) "ay"))
-     :else word))
+  word)
+
+
+(defn translate [word]
+  (cond
+    (vowel-sound? word) (str word "ay")
+    (consonant-sound-with-qu? word) word
+    (consonant-sound-with-y? word) (str word "ay")
+    (consonant-sound? word) (let [word-seq (split-consonant-from-word word)]
+                              (str (second word-seq) (first word-seq) "ay"))
+    :else word))
