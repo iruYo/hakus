f1b6104e036b449bd419e3b5aa4fe171b62126c7
Youri <youri.ulbrich@web.de>
2022-09-23 01:56:24 +0200

    Added in-line null check for rna-transcription

--- clojure/rna-transcription/src/rna_transcription/core.clj
+++ clojure/rna-transcription/src/rna_transcription/core.clj
@@ -1,11 +1,26 @@
 (ns rna-transcription.core)
 
-(def translation {\C "G"
+(def translations {\C "G"
                   \G "C"
                   \A "U"
                   \T "A"})
-(defn to-rna [dna]
-  (let [translation (map translation dna)]
+(defn to-rna3 [dna]
+  (->> dna
+       (map (fn [part]
+              (if-let [translation (translations part)]
+                translation
+                (throw (AssertionError.)))))
+       (apply str)))
+
+(defn to-rna2 [dna]
+  (let [translation (map translations dna)]
     (if-not (every? identity translation)
       (throw (AssertionError.))
       (apply str translation))))
+
+(defn to-rna [dna]
+  (when (some nil? (map translations dna))
+    (throw (AssertionError.)))
+  (->> dna
+       (map translations)
+       (apply str)))
