0c40e279d57e4409449822dcc2c81cc0750797b6
Youri <youri.ulbrich@web.de>
2022-09-30 04:47:13 +0200

    Implemented RLE

--- clojure/rle/src/rle/core.clj
+++ clojure/rle/src/rle/core.clj
@@ -1,5 +1,15 @@
 (ns rle.core)
 
+
+(defn run-length-encode2 [s]
+  (->> s
+       (partition-by identity)
+       (mapcat #(if (= 1 (count %))
+                  [(first %)]
+                  [(count %) (first %)]))
+       ;(apply str)
+       ))
+
 (defn run-length-encode "encodes a string with run-length-encoding" [plain-text]
   (->> plain-text
        (partition-by identity)
@@ -11,4 +21,10 @@
                           length))
                       (apply str (keys freq-map)))) "")))
 
-(defn run-length-decode "decodes a run-length-encoded string" [cipher-text])
+(defn run-length-decode "decodes a run-length-encoded string" [cipher-text]
+  (apply str (map (fn [[_ count letter]]
+                    (apply str (repeat (if (= count "")
+                                         1
+                                         (Integer/parseInt count))
+                                       letter)))
+                  (re-seq #"(\d*)([A-Za-z ]{1})" cipher-text))))
